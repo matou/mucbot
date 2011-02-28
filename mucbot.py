b87be1f20e320739d79b944c32526171022773de
matou <matou@taunusstein.net>
2011-02-28 10:16:49 +0100

    This should be documentation and not only a string.

--- mucbot.py
+++ mucbot.py
@@ -16,7 +16,7 @@ from threading import Thread
 class Mucbot(Thread):
     
     def __init__(self, jid, pwd, room, botname='', roompwd=''):
-        "initalize bot and let it join the room"
+        '''initalize bot and let it join the room'''
         Thread.__init__(self)
 
         if botname == '':
@@ -37,14 +37,14 @@ class Mucbot(Thread):
         self.client.send(p)
 
     def msg_rcv(self, sess, msg):
-        "will be executed when a message arrives"
+        '''will be executed when a message arrives'''
         pass
 
     def pres_rcv(self, sess, pres):
         pass
 
     def say(self, msg):
-        "send the given message to the room"
+        '''send the given message to the room'''
         m = xmpp.Message(to=self.room, body=msg, typ='groupchat')
         self.client.send(m)
 
